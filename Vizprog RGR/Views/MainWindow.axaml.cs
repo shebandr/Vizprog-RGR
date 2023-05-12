4b505e95b98044a377e6bc5af8d944248a50b010
Андрей Шебанов <shebanoff.andrei@yandex.ru>
2023-05-12 08:11:48 +0700

    Добавьте файлы проекта.

---
+++ Vizprog RGR/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Vizprog_RGR.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
