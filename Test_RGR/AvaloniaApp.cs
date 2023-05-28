ec6c3d80219ae9a984379eab84b325256a335115
Андрей Шебанов <shebanoff.andrei@yandex.ru>
2023-05-28 19:55:24 +0700

    added tests

--- Test_RGR/AvaloniaApp.cs
+++ Test_RGR/AvaloniaApp.cs
@@ -18,24 +18,28 @@ namespace Test_RGR
         public static void Stop()
         {
             var app = GetApp();
-            if (app is IDisposable disposable)
-            {
-                Dispatcher.UIThread.Post(disposable.Dispose);
-            }
+            if (app is IDisposable disposable) Dispatcher.UIThread.Post(disposable.Dispose);
 
             Dispatcher.UIThread.Post(() => app.Shutdown());
         }
 
-        public static MainWindow GetMainWindow() => (MainWindow)GetApp().MainWindow;
+        public static StartWindow GetMainWindow() => (StartWindow)GetApp().MainWindow;
 
-        public static IClassicDesktopStyleApplicationLifetime GetApp() =>
-            (IClassicDesktopStyleApplicationLifetime)Application.Current.ApplicationLifetime;
+        public static IClassicDesktopStyleApplicationLifetime GetApp()
+        {
+            var app = Application.Current ?? throw new Exception("Приложение не найдено");
+            var life = app.ApplicationLifetime ?? throw new Exception("Приложение не найдено");
+            return (IClassicDesktopStyleApplicationLifetime)life;
+        }
 
-        public static AppBuilder BuildAvaloniaApp() =>
-            AppBuilder
+        public static AppBuilder BuildAvaloniaApp()
+        {
+            App.lock_inc_build = true;
+            return AppBuilder
                 .Configure<App>()
                 .UsePlatformDetect()
                 .UseReactiveUI();
-
+                
+        }
     }
 }
